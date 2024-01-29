6df1572420ed7b6972415fba2294ce893c465be4
satnaing <satnaingdev@gmail.com>
2024-01-29 16:10:48 +0630

    refactor: update sidebar logo and title

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,7 +2,6 @@ import Nav from './nav'
 import { TooltipProvider } from './ui/tooltip'
 import { cn } from '@/lib/utils'
 import { Separator } from './ui/separator'
-import FaryLogo from '@/assets/vite.png'
 import { Button } from './ui/button'
 import { sidelinks } from '@/data/sidelinks'
 import { IconChevronsLeft } from '@tabler/icons-react'
@@ -26,16 +25,41 @@ export default function Sidebar({
     >
       <TooltipProvider delayDuration={0}>
         <div className='px-4 py-2 flex gap-2 h-16 items-center'>
-          <img
-            src={FaryLogo}
-            alt='Logo'
-            className={`transition-all ${isCollapsed ? 'h-6 w-6' : 'h-12 w-12'}`}
-          />
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 256 256'
+            className={`transition-all ${isCollapsed ? 'h-6 w-6' : 'h-8 w-8'}`}
+          >
+            <rect width='256' height='256' fill='none'></rect>
+            <line
+              x1='208'
+              y1='128'
+              x2='128'
+              y2='208'
+              fill='none'
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='16'
+            ></line>
+            <line
+              x1='192'
+              y1='40'
+              x2='40'
+              y2='192'
+              fill='none'
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='16'
+            ></line>
+            <span className='sr-only'>Website Name</span>
+          </svg>
           <div
             className={`flex flex-col justify-end truncate ${isCollapsed ? 'w-0 invisible' : 'w-auto visible'}`}
           >
-            <span className='font-medium'>Right Direction</span>
-            <span className='text-xs'>Since 2020</span>
+            <span className='font-medium'>Shadcn Dashboard</span>
+            <span className='text-xs'>Vite + ShadcnUI</span>
           </div>
         </div>
         <Separator className='mb-4' />
