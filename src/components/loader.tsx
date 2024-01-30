9f99b67cb9da2baea32c2f083619e67f20583c34
satnaing <satnaingdev@gmail.com>
2024-01-30 10:49:52 +0630

    feat: add loader component ui

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,3 +1,10 @@
+import { IconLoader } from '@tabler/icons-react'
+
 export default function Loader() {
-  return <span>loader</span>
+  return (
+    <div className='w-full h-svh flex justify-center items-center'>
+      <IconLoader className='animate-spin' size={32} />
+      <span className='sr-only'>loading</span>
+    </div>
+  )
 }
