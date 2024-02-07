105d582ad24c0c43b35e9a21d92da05ac702470e
satnaing <satnaingdev@gmail.com>
2024-02-07 14:15:03 +0630

    fix: exclude shadcn components from linting and remove unused props

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -63,6 +63,7 @@ export function ThemeProvider({
   )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => {
   const context = useContext(ThemeProviderContext)
 
