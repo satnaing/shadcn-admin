72810ddaf7de03f3909b502f4404846b38a118c3
satnaing <satnaingdev@gmail.com>
2024-02-08 11:00:32 +0630

    feat: update theme-color meta tag when theme is updated

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,9 +1,19 @@
 import { IconMoon, IconSun } from '@tabler/icons-react'
 import { useTheme } from './theme-provider'
 import { Button } from './custom/button'
+import { useEffect } from 'react'
 
 export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme()
+
+  /* Update theme-color meta tag
+   * when theme is updated */
+  useEffect(() => {
+    const themeColor = theme === 'dark' ? '#020817' : '#fff'
+    const metaThemeColor = document.querySelector("meta[name='theme-color']")
+    metaThemeColor && metaThemeColor.setAttribute('content', themeColor)
+  }, [theme])
+
   return (
     <Button
       size='icon'
