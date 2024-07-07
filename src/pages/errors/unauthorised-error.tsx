e10c930f36a375d8faef01437d6b9dd52c73d3ba
rakrisi <radhesingh12321@gmail.com>
2024-07-07 19:34:13 +0530

    code format

--- src/pages/errors/unauthorised-error.tsx
+++ src/pages/errors/unauthorised-error.tsx
@@ -7,9 +7,12 @@ export default function UnauthorisedError() {
     <div className='h-svh'>
       <div className='m-auto flex h-full w-full flex-col items-center justify-center gap-2'>
         <h1 className='text-[7rem] font-bold leading-tight'>401</h1>
-        <span className='font-medium'>Oops! You don't have permission to access this page.</span>
+        <span className='font-medium'>
+          Oops! You don't have permission to access this page.
+        </span>
         <p className='text-center text-muted-foreground'>
-          It looks like you tried to access a resource that requires proper authentication. <br />
+          It looks like you tried to access a resource that requires proper
+          authentication. <br />
           Please log in with the appropriate credentials.
         </p>
         <div className='mt-6 flex gap-4'>
