bc75eab31676e244987f41c987ed0a5a3900d1b8
Sat Naing <satnaingdev@gmail.com>
2024-09-22 08:19:40 +0700

    fix: solve asChild attribute issue in custom button (#31)

--- src/components/custom/button.tsx
+++ src/components/custom/button.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
-import { cva, type VariantProps } from 'class-variance-authority'
-
-import { cn } from '@/lib/utils'
 import { IconLoader2 } from '@tabler/icons-react'
+import { cva, type VariantProps } from 'class-variance-authority'
+import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
@@ -52,12 +51,13 @@ type ButtonProps = ButtonPropsBase &
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, children, ...props }, ref) => {
-    if (props.asChild) {
+    const { asChild, ...rest } = props
+    if (asChild) {
       return (
         <Slot
           className={cn(buttonVariants({ variant, size, className }))}
           ref={ref}
-          {...props}
+          {...rest}
         >
           {children}
         </Slot>
