5ef7b43b36a01d7451e9cff3a0fce47bfde3fbe7
Teo Wen Long <teowenlong0316@gmail.com>
2024-09-29 10:24:33 +0800

    feat: multiple language support

--- src/pages/errors/maintenance-error.tsx
+++ src/pages/errors/maintenance-error.tsx
@@ -1,17 +1,18 @@
 import { Button } from '@/components/custom/button'
+import { useTranslations } from 'use-intl'
 
 export default function MaintenanceError() {
+  const t = useTranslations('error')
   return (
     <div className='h-svh'>
-      <div className='m-auto flex h-full w-full flex-col items-center justify-center gap-2'>
+      <div className='flex flex-col items-center justify-center w-full h-full gap-2 m-auto'>
         <h1 className='text-[7rem] font-bold leading-tight'>503</h1>
-        <span className='font-medium'>Website is under maintenance!</span>
+        <span className='font-medium'>{t('maintenance_title')}</span>
         <p className='text-center text-muted-foreground'>
-          The site is not available at the moment. <br />
-          We'll be back online shortly.
+          {t.rich('maintenance_desc', { br: () => <br /> })}
         </p>
-        <div className='mt-6 flex gap-4'>
-          <Button variant='outline'>Learn more</Button>
+        <div className='flex gap-4 mt-6'>
+          <Button variant='outline'>{t('learn_more')}</Button>
         </div>
       </div>
     </div>
---
+++ src/translations/index.ts
@@ -0,0 +1,5 @@
+import * as en from './en.json'
+import * as zh from './zh.json'
+import * as ms from './ms.json'
+
+export default { en, zh, ms }
