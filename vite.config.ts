955b0c396fb62e32c845a03fea91916e6872b5cd
satnaing <satnaingdev@gmail.com>
2024-11-23 02:54:33 +0700

    build: replace react-router with tanstack packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
