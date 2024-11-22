0fc3a07333edb05a6ba6817043679b1a4d6a67b7
satnaing <satnaingdev@gmail.com>
2024-11-23 02:35:56 +0700

    chore: upgrade Vite, Prettier, and ESLint
    Upgraded Vite and its related packages. Also updated
    Prettier and ESLint to the latest versions and
    made necessary adjustments to configuration files.

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist', 'src/components/ui'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...pluginQuery.configs['flat/recommended'],
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-console': 'error',
+    },
+  }
+)
