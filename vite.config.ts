99a404e239e691966152fe0bc8aed1f2be85878a
coji <coji@techtalk.jp>
2024-12-15 23:38:58 +0900

    refactor: remove unused files and update type definitions for React Router

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import { reactRouter } from '@react-router/dev/vite'
-import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { getLoadContext } from './load-context'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    cloudflareDevProxy({ getLoadContext }),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [reactRouter(), tsconfigPaths()],
 })
