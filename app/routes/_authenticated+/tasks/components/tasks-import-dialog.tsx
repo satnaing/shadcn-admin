259287ce4732899179c2c1a6f6b1216d7a009447
coji <coji@techtalk.jp>
2024-12-16 12:51:42 +0900

    feat: added tasks

---
+++ app/routes/_authenticated+/tasks/components/tasks-import-dialog.tsx
@@ -0,0 +1,99 @@
+import { z } from 'zod'
+import { getFormProps, getInputProps, useForm } from '@conform-to/react'
+import { parseWithZod } from '@conform-to/zod'
+import { Form } from 'react-router'
+import { toast } from 'sonner'
+import { Button } from '~/components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '~/components/ui/dialog'
+import { Input } from '~/components/ui/input'
+import { Label } from '~/components/ui/label'
+
+const formSchema = z.object({
+  file: z
+    .instanceof(File)
+    .refine(
+      (file) => ['text/csv'].includes(file.type),
+      'Please upload csv format.'
+    ),
+})
+
+interface Props {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
+
+export function TasksImportDialog({ open, onOpenChange }: Props) {
+  const [form, { file }] = useForm<z.infer<typeof formSchema>>({
+    defaultValue: { file: undefined },
+    onValidate: ({ formData }) =>
+      parseWithZod(formData, { schema: formSchema }),
+    onSubmit: (event, { submission }) => {
+      event.preventDefault()
+      if (submission?.status !== 'success') return
+      const fileDetails = {
+        name: submission.value.file.name,
+        size: submission.value.file.size,
+        type: submission.value.file.type,
+      }
+
+      toast.success('You have imported the following file:', {
+        description: (
+          <div className='overflow-hidden'>
+            <div className='overflow-auto'>
+              <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+                <code className='text-white'>
+                  {JSON.stringify(fileDetails, null, 2)}
+                </code>
+              </pre>
+            </div>
+          </div>
+        ),
+      })
+      onOpenChange(false)
+    },
+  })
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={(val) => {
+        onOpenChange(val)
+        form.reset()
+      }}
+    >
+      <DialogContent className='sm:max-w-sm gap-2'>
+        <DialogHeader className='text-left'>
+          <DialogTitle>Import Tasks</DialogTitle>
+          <DialogDescription>
+            Import tasks quickly from a CSV file.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...getFormProps(form)}>
+          <div>
+            <Label htmlFor={file.id}>File</Label>
+            <Input {...getInputProps(file, { type: 'file' })} key={file.key} />
+            <div id={file.errorId} className='text-destructive text-sm'>
+              {file.errors}
+            </div>
+          </div>
+        </Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant='outline'>Close</Button>
+          </DialogClose>
+          <Button type='submit' form={form.id}>
+            Import
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
