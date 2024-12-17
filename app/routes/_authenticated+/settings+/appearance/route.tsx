a2682efe2b4e4c965ceae4525c07dbc90e5c674e
coji <coji@techtalk.jp>
2024-12-17 17:03:11 +0900

    feat: add appearance settings form for theme and font customization

---
+++ app/routes/_authenticated+/settings+/appearance/route.tsx
@@ -0,0 +1,16 @@
+import ContentSection from '../_layout/components/content-section'
+import { AppearanceForm } from './appearance-form'
+
+// import { AppearanceForm } from './appearance-form'
+
+export default function SettingsAppearance() {
+  return (
+    <ContentSection
+      title='Appearance'
+      desc='Customize the appearance of the app. Automatically switch between day
+          and night themes.'
+    >
+      <AppearanceForm />
+    </ContentSection>
+  )
+}
