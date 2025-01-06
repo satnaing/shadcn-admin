8dc38e500fd7ed8eb788c288bdc663f35e82abc5
Sullivan <38718448+Epic-R-R@users.noreply.github.com>
2025-01-06 05:46:02 +0330

    fix: toolbar filter placeholder typo in user table (#76)

--- src/features/users/components/data-table-toolbar.tsx
+++ src/features/users/components/data-table-toolbar.tsx
@@ -19,7 +19,7 @@ export function DataTableToolbar<TData>({
     <div className='flex items-center justify-between'>
       <div className='flex flex-1 flex-col-reverse items-start gap-y-2 sm:flex-row sm:items-center sm:space-x-2'>
         <Input
-          placeholder='Filter tasks...'
+          placeholder='Filter users...'
           value={
             (table.getColumn('username')?.getFilterValue() as string) ?? ''
           }
