1c1648cd624a4f7b9a1e73f0b036975296f2e8ff
Hr-Mohammadian <dr.a3del@gmail.com>
2025-01-10 19:48:40 +0100

    added side bar menus

---
+++ README.md
@@ -0,0 +1,25 @@
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/satnaing/shadcn-admin.git
+```
+
+Go to the project directory
+
+```bash
+  cd shadcn-admin
+```
+
+Install dependencies
+
+```bash
+  pnpm install
+```
+
+Start the server
+
+```bash
+  pnpm run dev
+```
--- src/components/layout/team-switcher.tsx
+++ src/components/layout/team-switcher.tsx
@@ -60,7 +60,7 @@ export function TeamSwitcher({
             sideOffset={4}
           >
             <DropdownMenuLabel className='text-xs text-muted-foreground'>
-              Teams
+              Tenants
             </DropdownMenuLabel>
             {teams.map((team, index) => (
               <DropdownMenuItem
@@ -84,7 +84,7 @@ export function TeamSwitcher({
               <div className='flex size-6 items-center justify-center rounded-md border bg-background'>
                 <Plus className='size-4' />
               </div>
-              <div className='font-medium text-muted-foreground'>Add team</div>
+              <div className='font-medium text-muted-foreground'>Add Tenant</div>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
