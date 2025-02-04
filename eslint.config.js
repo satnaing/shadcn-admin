c5a44a69fbe129ff1d5d721aae61804ed1aafdbd
satnaing <satnaingdev@gmail.com>
2025-02-04 12:57:27 +0700

    chore: update ts-unused-vars rule in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -29,6 +29,19 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       'no-console': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   }
 )
