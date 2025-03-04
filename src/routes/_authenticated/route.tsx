bee33b0da9113eeff6264642631c5855f88ce951
iKowalchuk <yaroslav.kowalchuk@gmail.com>
2025-03-04 11:13:57 +0200

    style: enhance sidebar content width and responsiveness

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,5 +1,6 @@
 import Cookies from 'js-cookie'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { cn } from '@/lib/utils'
 import { SearchProvider } from '@/context/search-context'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/layout/app-sidebar'
@@ -14,7 +15,18 @@ function RouteComponent() {
     <SearchProvider>
       <SidebarProvider defaultOpen={defaultOpen}>
         <AppSidebar />
-        <SidebarInset>
+        <SidebarInset
+          id='content'
+          className={cn(
+            'ml-auto w-full max-w-full',
+            'peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]',
+            'peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]',
+            'transition-[width] duration-200 ease-linear',
+            'flex h-svh flex-col',
+            'group-data-[scroll-locked=1]/body:h-full',
+            'group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh'
+          )}
+        >
           <Outlet />
         </SidebarInset>
       </SidebarProvider>
