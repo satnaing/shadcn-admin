92b8b2c55fe8ad6d5f92b7fef6f3b2ff415add4e
obtuse-triangle <me@obtuse.kr>
2025-03-29 21:43:06 +0900

    no-console error to warn

--- eslint.config.js
+++ eslint.config.js
@@ -28,7 +28,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'no-console': 'error',
+      'no-console': 'warn',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
