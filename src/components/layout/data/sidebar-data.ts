85c92ca987b89f765a1e5003cacf75c45e5f1a33
galbimandu <galbimandu1126@gmail.com>
2025-03-29 19:35:03 +0900

    chore : title 수정

--- src/components/layout/data/sidebar-data.ts
+++ src/components/layout/data/sidebar-data.ts
@@ -30,7 +30,7 @@ export const sidebarData: SidebarData = {
   },
   teams: [
     {
-      name: 'Shadcn Admin',
+      name: 'BSMHUB Admin',
       logo: Command,
       plan: 'Vite + ShadcnUI',
     },
