d012efece8bf9c1c09bf0ada0d968d8e2be6d4dd
galbimandu <galbimandu1126@gmail.com>
2025-03-31 08:28:05 +0900

    add : 삽입 성공 시 toast 추가

--- src/features/companies/services/insertCompany.tsx
+++ src/features/companies/services/insertCompany.tsx
@@ -1,6 +1,7 @@
 import supabase from "@/utils/supabase/client";
 import { CompanySupabase } from "../data/schema";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useToast } from "@/hooks/use-toast";
 
 export const insertCompany = async (newCompany: {
   newCompany: Pick<CompanySupabase, "company_name">;
@@ -9,6 +10,7 @@ export const insertCompany = async (newCompany: {
     .schema("enum")
     .from("companies")
     .insert([newCompany.newCompany])
+    .select("*")
     .single();
 
   if (error) {
@@ -21,12 +23,18 @@ export const insertCompany = async (newCompany: {
 
 export const useInsertCompanyMutation = () => {
   const queryClient = useQueryClient();
+  const { toast } = useToast();
 
   const mutation = useMutation({
     mutationFn: insertCompany,
     onSuccess: (data) => {
       console.log("Company inserted successfully:", data);
       queryClient.invalidateQueries({ queryKey: ['companies'] });
+      toast({
+        variant: "default",
+        title: '데이터 삽입 성공!',
+        description: `회사: ${data.company_name}`,
+      })
     },
     onError: (error) => {
       console.error("Error inserting company:", error);
