f140d54c4e8cd6c7ae54ea1405679db073431dcd
obtuse-triangle <me@obtuse.kr>
2025-04-03 00:32:54 +0900

    Define Warning Color in index.css, tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,6 +59,10 @@ export default {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
         },
+        warning: {
+          DEFAULT: 'hsl(var(--warning))',
+          foreground: 'hsl(var(--warning-foreground))',
+        },
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
