fb9e0b9323495cec0ee9c3285c3be739c53f23fe
UncleBigBay <ayodelesamueladebayo143@gmail.com>
2025-04-12 04:57:25 +0100

    feat: add loading indicator during page transitions (#119)
    * feat: add loading indicator during page transitions
    
    * fix: update loading bar color
    
    ---------
    Co-authored-by: satnaing <satnaingdev@gmail.com>

---
+++ src/components/navigation-progress.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react'
+import { useRouterState } from '@tanstack/react-router'
+import LoadingBar, { LoadingBarRef } from 'react-top-loading-bar'
+
+export function NavigationProgress() {
+  const ref = useRef<LoadingBarRef>(null)
+  const state = useRouterState()
+
+  useEffect(() => {
+    if (state.status === 'pending') {
+      ref.current?.continuousStart()
+    } else {
+      ref.current?.complete()
+    }
+  }, [state.status])
+
+  return (
+    <LoadingBar
+      color='hsl(var(--muted-foreground))'
+      ref={ref}
+      shadow={true}
+      height={2}
+    />
+  )
+}
