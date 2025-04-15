acda950acbe506ea649c1baa93ee549096bc3045
fitimbytyqi <fitimbytyqi05@gmail.com>
2025-04-16 01:42:00 +0200

    bump: tanstack-router and router-plugin versions fix: install devtools as dependencies (https://github.com/TanStack/router/pull/3994) fix: change the order of plugins as per router docs suggestion

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     TanStackRouterVite({
       target: 'react',
       autoCodeSplitting: true,
     }),
+    react(),
   ],
   resolve: {
     alias: {
