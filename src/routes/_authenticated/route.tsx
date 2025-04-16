7a75c83991de30ec46304e84482efa8ab0b5e0ce
Sat Naing <satnaingdev@gmail.com>
2025-04-16 15:48:32 +0700

    fix: disable layout animation on mobile (#130)

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -23,7 +23,7 @@ function RouteComponent() {
             'ml-auto w-full max-w-full',
             'peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]',
             'peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]',
-            'transition-[width] duration-200 ease-linear',
+            'sm:transition-[width] sm:duration-200 sm:ease-linear',
             'flex h-svh flex-col',
             'group-data-[scroll-locked=1]/body:h-full',
             'has-[main.fixed-main]:group-data-[scroll-locked=1]/body:h-svh'
