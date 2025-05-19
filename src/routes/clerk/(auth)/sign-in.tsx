c1cf9fc24d1950236f5c59853dcb839cdf1f4a7c
satnaing <satnaingdev@gmail.com>
2025-05-19 18:58:17 +0700

    fix: add default email address in clerk sign in

--- src/routes/clerk/(auth)/sign-in.tsx
+++ src/routes/clerk/(auth)/sign-in.tsx
@@ -4,6 +4,11 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export const Route = createFileRoute('/clerk/(auth)/sign-in')({
   component: () => (
-    <SignIn fallback={<Skeleton className='h-[30rem] w-[25rem]' />} />
+    <SignIn
+      initialValues={{
+        emailAddress: 'your_mail+shadcn_admin@gmail.com',
+      }}
+      fallback={<Skeleton className='h-[30rem] w-[25rem]' />}
+    />
   ),
 })
