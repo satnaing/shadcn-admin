b0a985168beb52083e246a49f72cf842ca05cda5
satnaing <satnaingdev@gmail.com>
2025-05-19 20:52:05 +0700

    chore: add learn more button in clerk auth layout

--- src/routes/clerk/(auth)/route.tsx
+++ src/routes/clerk/(auth)/route.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
 import { ClerkFullLogo } from '@/assets/clerk-full-logo'
+import { LearnMore } from '@/components/learn-more'
 
 export const Route = createFileRoute('/clerk/(auth)')({
   component: ClerkAuthLayout,
@@ -42,7 +43,24 @@ function ClerkAuthLayout() {
         </div>
       </div>
       <div className='lg:p-8'>
-        <div className='mx-auto flex w-full flex-col items-center justify-center gap-4'>
+        <div className='relative mx-auto flex w-full flex-col items-center justify-center gap-4'>
+          <LearnMore
+            defaultOpen
+            triggerProps={{
+              className: 'absolute -top-12 right-0 sm:right-20 size-6',
+            }}
+            contentProps={{ side: 'top', align: 'end', className: 'w-auto' }}
+          >
+            Welcome to the example Clerk auth page. <br />
+            Back to{' '}
+            <Link
+              to='/'
+              className='underline decoration-dashed underline-offset-2'
+            >
+              Dashboard
+            </Link>{' '}
+            ?
+          </LearnMore>
           <Outlet />
         </div>
       </div>
