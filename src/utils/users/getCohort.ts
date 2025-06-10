25189364708c5ce8990614d954c63486a74bec07
kwon5700 <kwon777071@gmail.com>
2025-06-11 03:22:13 +0900

    fix: 학생 기수 매핑 함수 변경

--- src/utils/users/getCohort.ts
+++ src/utils/users/getCohort.ts
@@ -1,22 +1,10 @@
-const userStatus: Record<number, string> = {
-  0: '1학년',
-  1: '2학년',
-  3: '3학년'
-}
+const SCHOOL_FOUNDING_YEAR = 2021
 
 export const getCohort = (join_at: string): string => {
   const joinDate = new Date(join_at)
-  const now = new Date()
-
   const joinYear  = joinDate.getFullYear()
-  const nowYear = now.getFullYear()
-  const nowMonth = now.getMonth() + 1
 
-  let status = nowYear - joinYear
+  const cohort = joinYear - SCHOOL_FOUNDING_YEAR
 
-  if (nowMonth < 3) {
-    status -= 1
-  }
-
-  return userStatus[status] ?? '졸업생'
+  return `${String(cohort)}기`
 }
