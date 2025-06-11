379b50cd1c85c9655a83d65a7c52f1dcc873a1d6
kwon5700 <kwon777071@gmail.com>
2025-06-12 00:46:22 +0900

    fix: 학생 기수 함수 수정

--- src/utils/users/getCohort.ts
+++ src/utils/users/getCohort.ts
@@ -4,7 +4,7 @@ export const getCohort = (join_at: string): string => {
   const joinDate = new Date(join_at)
   const joinYear  = joinDate.getFullYear()
 
-  const cohort = joinYear - SCHOOL_FOUNDING_YEAR
+  const cohort = joinYear - SCHOOL_FOUNDING_YEAR + 1
 
   return `${String(cohort)}기`
 }
