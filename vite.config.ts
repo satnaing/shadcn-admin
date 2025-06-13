c2653b7826f2071a13b827a47fdec31e908ee0f5
Eelco Wiersma <eelco@appulse.nl>
2025-06-13 09:59:40 +0100

    chore: add diffnote

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { DiffnoteVitePlugin } from 'diffnote/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,22 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    DiffnoteVitePlugin({
+      projectKey: process.env.DIFFNOTE_PROJECT_KEY ?? 'akYk5S-QHt0HCaMWU4rIK',
+      branch:
+        process.env.NODE_ENV === 'development'
+          ? 'dev'
+          : process.env.CF_PAGES_BRANCH,
+      productionBranch: 'main',
+    }),
   ],
+  define: {
+    __DIFFNOTE_PREVIEW_BRANCH__:
+      process.env.CF_PAGES_BRANCH !== 'main'
+        ? process.env.CF_PAGES_BRANCH
+        : undefined,
+  },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
