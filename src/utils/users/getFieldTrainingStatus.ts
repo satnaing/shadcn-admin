701edea61dc7b5d1c7d66f7c4428fb8d1d05989e
kwon5700 <kwon777071@gmail.com>
2025-06-14 04:42:00 +0900

    fix: 현장 실습 종료일이 나오기 전까지 임시로 변경

--- src/utils/users/getFieldTrainingStatus.ts
+++ src/utils/users/getFieldTrainingStatus.ts
@@ -8,10 +8,11 @@ export const fieldTrainingStatus: Record<number, string> = {
 export const getFieldTrainingStatus = (field_training: FieldTrainingType[]) => {
   const now = new Date()
 
-  const isFieldTraining = field_training.some(({ end_date }) => {
+  const isFieldTraining = field_training.some(({ start_date, end_date }) => {
+    const startDate = new Date(start_date)
     const endDate = new Date(end_date)
     
-    return now <= endDate
+    return now <= endDate || startDate
   })
 
   return fieldTrainingStatus[Number(isFieldTraining)]
