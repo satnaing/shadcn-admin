7741d5092ecc6dbebec6e0668c9349ad7d311abe
jaehwan02 <jhrick0129@gmail.com>
2025-06-16 21:49:27 +0900

    feat : 로그인페이지 한글화

--- src/features/auth/sign-in/index.tsx
+++ src/features/auth/sign-in/index.tsx
@@ -7,29 +7,28 @@ export default function SignIn() {
     <AuthLayout>
       <Card className='p-6'>
         <div className='flex flex-col space-y-2 text-left'>
-          <h1 className='text-2xl font-semibold tracking-tight'>Login</h1>
+          <h1 className='text-2xl font-semibold tracking-tight'>로그인</h1>
           <p className='text-sm text-muted-foreground'>
-            Enter your email and password below <br />
-            to log into your account
+            계정에 로그인하려면 아래에 로그인 버튼을 클릭하세요.
           </p>
         </div>
         <UserAuthForm />
         <p className='mt-4 px-8 text-center text-sm text-muted-foreground'>
-          By clicking login, you agree to our{' '}
+          로그인 버튼을 클릭하면{' '}
           <a
             href='/terms'
             className='underline underline-offset-4 hover:text-primary'
           >
-            Terms of Service
+            서비스 약관
           </a>{' '}
-          and{' '}
+          및{' '}
           <a
             href='/privacy'
             className='underline underline-offset-4 hover:text-primary'
           >
-            Privacy Policy
+            개인정보 처리방침
           </a>
-          .
+          에 동의하게 됩니다.
         </p>
       </Card>
     </AuthLayout>
