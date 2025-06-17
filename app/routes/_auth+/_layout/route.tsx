6dc1981630bae5719f1adb4d8ecbfef4a32c870e
coji <coji@techtalk.jp>
2025-06-17 18:26:56 +0900

    fix: adjust layout structure for improved responsiveness

--- app/routes/_auth+/_layout/route.tsx
+++ app/routes/_auth+/_layout/route.tsx
@@ -7,8 +7,8 @@ export default function AuthLayout() {
   }
 
   return (
-    <div className="bg-primary-foreground container grid h-svh flex-col items-center justify-center lg:max-w-none lg:px-0">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8">
+    <div className="bg-primary-foreground grid h-svh">
+      <div className="container mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8">
         <div className="mb-4 flex items-center justify-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
