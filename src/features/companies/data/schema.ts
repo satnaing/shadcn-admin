dd19bca411e142b57487b15303bc0d83de12801a
galbimandu <galbimandu1126@gmail.com>
2025-06-17 23:48:48 +0900

    chore : parts of company form nullable

--- src/features/companies/data/schema.ts
+++ src/features/companies/data/schema.ts
@@ -4,12 +4,11 @@ import { Database } from '@/utils/supabase/database.types'
 export const companySchema = z.object({
   company_id: z.number(),
   company_name: z.string().min(1, { message: '회사이름을 입력해주세요.' }),
-  hr_manager_name: z.string().min(1, { message: '인사 담당자 이름을 입력해주세요.' }),
-  hr_manager_phone: z.string().min(1, { message: '인사 담당자 연락처를 입력해주세요.' }),
-  company_address: z.string().min(1, { message: '회사 주소를 입력해주세요.' }),
+  hr_manager_name: z.string().nullable(),
+  hr_manager_phone: z.string().nullable(),
+  company_address: z.string().nullable(),
   isEdit: z.boolean(),
 })
-
 
 export const companyFormSchema = companySchema.omit({ company_id: true })
 export const companyWithoutEdit = companySchema.omit({ isEdit: true })
@@ -20,16 +19,25 @@ export type CompanyForm = z.infer<typeof companyFormSchema>
 export type CompanyFormFieldType = keyof Omit<Company, 'company_id' | 'isEdit'>
 
 export const companyFieldMetadata: Record<
-CompanyFormFieldType, 
-{ label: string; placeholder: string; isRepresentative?: boolean }
+  CompanyFormFieldType,
+  { label: string; placeholder: string; isRepresentative?: boolean }
 > = {
-  company_name: { label: '회사 이름', placeholder: '예: ABC 주식회사', isRepresentative: true },
+  company_name: {
+    label: '회사 이름',
+    placeholder: '예: ABC 주식회사',
+    isRepresentative: true,
+  },
   hr_manager_name: { label: '인사 담당자 이름', placeholder: '예: 홍길동' },
-  hr_manager_phone: { label: '인사 담당자 연락처', placeholder: '예: 010-1234-5678' },
+  hr_manager_phone: {
+    label: '인사 담당자 연락처',
+    placeholder: '예: 010-1234-5678',
+  },
   company_address: { label: '회사 주소', placeholder: '예: 서울특별시 강남구' },
 }
 
-export const COMPANY_REPRESENTATIVE_FIELD = Object.entries(companyFieldMetadata)
-  .find(([_, metadata]) => metadata.isRepresentative === true)?.[0] ?? Object.keys(companyFieldMetadata)[0];
+export const COMPANY_REPRESENTATIVE_FIELD =
+  Object.entries(companyFieldMetadata).find(
+    ([_, metadata]) => metadata.isRepresentative === true
+  )?.[0] ?? Object.keys(companyFieldMetadata)[0]
 
 export type CompanySupabase = Database['public']['Tables']['companies']['Row']
