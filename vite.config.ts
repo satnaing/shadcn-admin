5d347c9b024368bd57b9181e932b8e8e93749552
coji <coji@techtalk.jp>
2025-06-17 17:41:34 +0900

    feat: add vite-plugin-devtools-json to enhance development experience

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import devtoolsJson from 'vite-plugin-devtools-json'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [devtoolsJson(), tailwindcss(), reactRouter(), tsconfigPaths()],
 })
