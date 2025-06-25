36584dcb31c7d43458311ea8f08f94d6839ec709
Deep <monideep.purkayastha@stashfin.com>
2025-06-25 13:13:19 +0530

    Bug changes

--- src/routes/_authenticated/bbps/index.tsx
+++ src/routes/_authenticated/bbps/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import BBPS from '@/features/BBPS'
+import BBPS from '@/features/bbps'
 import { ProtectedRoute } from '@/components/ProtectedRoute'
 
 export const Route = createFileRoute('/_authenticated/bbps/')({
