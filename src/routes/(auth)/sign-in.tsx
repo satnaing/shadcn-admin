86cbd805a0802048ac41a96af4cd7c615f4c1480
Deep <monideep.purkayastha@stashfin.com>
2025-06-25 13:27:49 +0530

    fix sign in

--- src/routes/(auth)/sign-in.tsx
+++ src/routes/(auth)/sign-in.tsx
@@ -1,6 +1,12 @@
+import { z } from 'zod'
 import { createFileRoute } from '@tanstack/react-router'
+// <-- add this import
 import SignIn from '@/features/auth/sign-in'
 
 export const Route = createFileRoute('/(auth)/sign-in')({
+  // Add this:
+  validateSearch: z.object({
+    redirect: z.string().optional(),
+  }),
   component: SignIn,
 })
