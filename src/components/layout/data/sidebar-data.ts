dd2ab5b8ca047e75fd0f47a899120e9fa4cdfbf4
Satyam Yadav <satyamok03@gmail.com>
2025-07-14 22:33:21 +0530

    # clean comments in sidebar, fonts

--- src/components/layout/data/sidebar-data.ts
+++ src/components/layout/data/sidebar-data.ts
@@ -1,23 +1,12 @@
-import {
-  // IconBarrierBlock,
-  // IconBug,
-  // IconChartCandle,  
+import { 
   IconChecklist,
-  // IconError404,
-  // IconHelp,
   IconLayoutDashboard,
-  // IconLock,
-  // IconLockAccess,
-  // IconMessages,
   IconNotification,
   IconPackages,
   IconPalette,
-  // IconServerOff,
   IconSettings,
   IconTool,
   IconUserCog,
-  // IconUserOff,
-  // IconUsers,
 } from '@tabler/icons-react'
 import { AudioWaveform, Command, GalleryVerticalEnd } from 'lucide-react'
 import { type SidebarData } from '../types'
@@ -65,17 +54,6 @@ export const sidebarData: SidebarData = {
           url: '/apps',
           icon: IconPackages,
         },
-        // {
-        //   title: 'Chats',
-        //   url: '/chats',
-        //   badge: '3',
-        //   icon: IconMessages,
-        // },
-        // {
-        //   title: 'Join Community',
-        //   url: '/users',
-        //   icon: IconUsers,
-        // },
          {
           title: 'Settings',
           icon: IconSettings,
@@ -103,116 +81,7 @@ export const sidebarData: SidebarData = {
             
           ],
         },
-        // {
-        //   title: 'Onboard',
-        //   url: '/onboarding',
-        //   icon: IconChartCandle,
-        // },
       ],
     },
-    // {
-    //   title: 'Pages',
-    //   items: [
-    //     {
-    //       title: 'Auth',
-    //       icon: IconLockAccess,
-    //       items: [
-    //         {
-    //           title: 'Sign In',
-    //           url: '/sign-in',
-    //         },
-    //         {
-    //           title: 'Sign In (2 Col)',
-    //           url: '/sign-in-2',
-    //         },
-    //         {
-    //           title: 'Sign Up',
-    //           url: '/sign-up',
-    //         },
-    //         {
-    //           title: 'Forgot Password',
-    //           url: '/forgot-password',
-    //         },
-    //         {
-    //           title: 'OTP',
-    //           url: '/otp',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'Errors',
-    //       icon: IconBug,
-    //       items: [
-    //         {
-    //           title: 'Unauthorized',
-    //           url: '/401',
-    //           icon: IconLock,
-    //         },
-    //         {
-    //           title: 'Forbidden',
-    //           url: '/403',
-    //           icon: IconUserOff,
-    //         },
-    //         {
-    //           title: 'Not Found',
-    //           url: '/404',
-    //           icon: IconError404,
-    //         },
-    //         {
-    //           title: 'Internal Server Error',
-    //           url: '/500',
-    //           icon: IconServerOff,
-    //         },
-    //         {
-    //           title: 'Maintenance Error',
-    //           url: '/503',
-    //           icon: IconBarrierBlock,
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
-    
-    // {
-    //   title: 'Other',
-    //   items: [
-    //     {
-    //       title: 'Settings',
-    //       icon: IconSettings,
-    //       items: [
-    //         {
-    //           title: 'Profile',
-    //           url: '/settings',
-    //           icon: IconUserCog,
-    //         },
-    //         {
-    //           title: 'Account',
-    //           url: '/settings/account',
-    //           icon: IconTool,
-    //         },
-    //         {
-    //           title: 'Appearance',
-    //           url: '/settings/appearance',
-    //           icon: IconPalette,
-    //         },
-    //         {
-    //           title: 'Notifications',
-    //           url: '/settings/notifications',
-    //           icon: IconNotification,
-    //         },
-    //         {
-    //           title: 'Display',
-    //           url: '/settings/display',
-    //           icon: IconBrowserCheck,
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'Help Center',
-    //       url: '/help-center',
-    //       icon: IconHelp,
-    //     },
-    //   ],
-    // },
   ],
 }
