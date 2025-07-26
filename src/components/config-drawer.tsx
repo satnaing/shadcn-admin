4d20cad0ec8f295e6302c0f44008f4568c12bd1c
satnaing <satnaingdev@gmail.com>
2025-07-26 16:59:47 +0700

    feat: update layout config style with custom icons

--- src/components/config-drawer.tsx
+++ src/components/config-drawer.tsx
@@ -1,8 +1,11 @@
+import * as RadioGroup from '@radix-ui/react-radio-group'
 import { Settings } from 'lucide-react'
+import { IconLayoutFloating } from '@/assets/custom/icon-layout-floating'
+import { IconLayoutInset } from '@/assets/custom/icon-layout-inset'
+import { IconLayoutSidebar } from '@/assets/custom/icon-layout-sidebar'
+import { cn } from '@/lib/utils'
 import { useLayout } from '@/context/layout-context'
 import { Button } from '@/components/ui/button'
-import { Label } from '@/components/ui/label'
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import {
   Sheet,
   SheetContent,
@@ -14,7 +17,6 @@ import {
 } from '@/components/ui/sheet'
 
 export function ConfigDrawer() {
-  const { variant, setVariant, collapsible, setCollapsible } = useLayout()
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -27,25 +29,9 @@ export function ConfigDrawer() {
           <SheetTitle>Theme config</SheetTitle>
           <SheetDescription>Theme config description</SheetDescription>
         </SheetHeader>
-        <div className='p-4'>
-          <div>
-            <h2 className='mb-2'>Sidebar</h2>
-            <RadioGroup value={variant} onValueChange={setVariant}>
-              <div className='flex items-center gap-3'>
-                <RadioGroupItem value='inset' id='inset' />
-                <Label htmlFor='inset'>Inset</Label>
-              </div>
-              <div className='flex items-center gap-3'>
-                <RadioGroupItem value='floating' id='floating' />
-                <Label htmlFor='floating'>Float</Label>
-              </div>
-              <div className='flex items-center gap-3'>
-                <RadioGroupItem value='sidebar' id='sidebar' />
-                <Label htmlFor='sidebar'>Sidebar</Label>
-              </div>
-            </RadioGroup>
-          </div>
-          <div>
+        <div className='space-y-6 p-4'>
+          <LayoutConfig />
+          {/* <div>
             <h2 className='mt-4 mb-2'>Collapsible</h2>
             <RadioGroup value={collapsible} onValueChange={setCollapsible}>
               <div className='flex items-center gap-3'>
@@ -57,7 +43,7 @@ export function ConfigDrawer() {
                 <Label htmlFor='offcanvas'>Offcanvas</Label>
               </div>
             </RadioGroup>
-          </div>
+          </div> */}
         </div>
         <SheetFooter className='gap-2'>
           <Button variant='destructive' type='submit'>
@@ -66,5 +52,53 @@ export function ConfigDrawer() {
         </SheetFooter>
       </SheetContent>
     </Sheet>
+  )
+}
+
+function LayoutConfig() {
+  const { variant, setVariant } = useLayout()
+  return (
+    <div>
+      <div className='text-muted-foreground mb-2 text-sm font-semibold'>
+        Layout
+      </div>
+      <RadioGroup.Root
+        value={variant}
+        onValueChange={setVariant}
+        className='grid w-full max-w-md grid-cols-3 gap-4'
+      >
+        {[
+          {
+            value: 'inset',
+            label: 'Inset',
+            icon: IconLayoutInset,
+          },
+          {
+            value: 'floating',
+            label: 'Floating',
+            icon: IconLayoutFloating,
+          },
+          {
+            value: 'sidebar',
+            label: 'Sidebar',
+            icon: IconLayoutSidebar,
+          },
+        ].map((option) => (
+          <RadioGroup.Item
+            key={option.value}
+            value={option.value}
+            className={cn(
+              'group',
+              'transition duration-200 ease-in',
+              'ring-border rounded-[6px] ring-[1px]',
+              'data-[state=checked]:ring-primary data-[state=checked]:shadow-2xl'
+            )}
+          >
+            <option.icon className='stroke-primary fill-primary group-data-[state=unchecked]:stroke-muted-foreground group-data-[state=unchecked]:fill-muted-foreground mask-clip-border' />
+            <span className='sr-only'>{option.label}</span>
+          </RadioGroup.Item>
+        ))}
+      </RadioGroup.Root>
+    </div>
   )
 }
