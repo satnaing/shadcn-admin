15babd3f5fc96efbf08fe70a4538490feef99aba
satnaing <satnaingdev@gmail.com>
2025-08-31 12:01:44 +0700

    refactor: change SidebarInset component from 'main' to 'div'

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -306,9 +306,9 @@ function SidebarRail({ className, ...props }: React.ComponentProps<'button'>) {
   )
 }
 
-function SidebarInset({ className, ...props }: React.ComponentProps<'main'>) {
+function SidebarInset({ className, ...props }: React.ComponentProps<'div'>) {
   return (
-    <main
+    <div
       data-slot='sidebar-inset'
       className={cn(
         'bg-background relative flex w-full flex-1 flex-col',
