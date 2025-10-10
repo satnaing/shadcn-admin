3d1ac5feeee2ad8e63d6c0df114fac2a781ae215
jaycodev <jason.vilac@gmail.com>
2025-10-10 11:32:06 -0500

    chore: apply Prettier formatting to destructive sign-out button

--- src/components/layout/nav-user.tsx
+++ src/components/layout/nav-user.tsx
@@ -106,7 +106,10 @@ export function NavUser({ user }: NavUserProps) {
                 </DropdownMenuItem>
               </DropdownMenuGroup>
               <DropdownMenuSeparator />
-              <DropdownMenuItem variant='destructive' onClick={() => setOpen(true)}>
+              <DropdownMenuItem
+                variant='destructive'
+                onClick={() => setOpen(true)}
+              >
                 <LogOut />
                 Sign out
               </DropdownMenuItem>
