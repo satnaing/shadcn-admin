a04f170e91d34ec95a2e041fc1c1eae5c2c09ee3
satnaing <satnaingdev@gmail.com>
2025-11-06 20:02:04 +0700

    fix(style): update data attribute class in authenticated layout

--- src/components/layout/authenticated-layout.tsx
+++ src/components/layout/authenticated-layout.tsx
@@ -26,11 +26,11 @@ export function AuthenticatedLayout({ children }: AuthenticatedLayoutProps) {
 
               // If layout is fixed, set the height
               // to 100svh to prevent overflow
-              'has-[[data-layout=fixed]]:h-svh',
+              'has-data-[layout=fixed]:h-svh',
 
               // If layout is fixed and sidebar is inset,
               // set the height to 100svh - spacing (total margins) to prevent overflow
-              'peer-data-[variant=inset]:has-[[data-layout=fixed]]:h-[calc(100svh-(var(--spacing)*4))]'
+              'peer-data-[variant=inset]:has-data-[layout=fixed]:h-[calc(100svh-(var(--spacing)*4))]'
             )}
           >
             {children ?? <Outlet />}
