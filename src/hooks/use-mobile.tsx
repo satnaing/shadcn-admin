9e90d2d180e29ae0d7d633a87fc2d81981e21bd5
satnaing <satnaingdev@gmail.com>
2026-02-04 06:20:19 +0700

    refactor: extract mobile query into a constant

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react'
 
 const MOBILE_BREAKPOINT = 768
+const MOBILE_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
 
 export function useIsMobile() {
   return React.useSyncExternalStore(
     (callback) => {
-      const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-      mql.addEventListener("change", callback)
-      return () => mql.removeEventListener("change", callback)
+      const mql = window.matchMedia(MOBILE_QUERY)
+      mql.addEventListener('change', callback)
+      return () => mql.removeEventListener('change', callback)
     },
-    () => window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`).matches,
+    () => window.matchMedia(MOBILE_QUERY).matches,
     () => false
   )
 }
