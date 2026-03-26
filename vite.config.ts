a8b5db0560611fec6c8d6b2b038b968310585d4f
satnaing <satnaingdev@gmail.com>
2026-03-26 22:29:02 +0700

    build(vite): switch to @vitejs/plugin-react
    Replace `@vitejs/plugin-react-swc` with
    `@vitejs/plugin-react` and update Vite config to match.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
