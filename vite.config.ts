b13f87d48da0c19daaf95c00b702a50e3e957111
satnaing <satnaingdev@gmail.com>
2026-04-13 12:16:31 +0700

    feat(tests): setup Vitest for unit/integration testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+/// <reference types="vitest/config" />
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { playwright } from '@vitest/browser-playwright'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +19,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [{ browser: 'chromium' }],
     },
   },
 })
