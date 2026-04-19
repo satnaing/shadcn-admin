1d578ca956221e2650c70d60c48058bdd2b791f0
satnaing <satnaingdev@gmail.com>
2026-04-19 13:08:34 +0700

    refactor(showSubmittedData): remove unused comment

--- src/lib/show-submitted-data.tsx
+++ src/lib/show-submitted-data.tsx
@@ -6,7 +6,6 @@ export function showSubmittedData(
 ) {
   toast.message(title, {
     description: (
-      // w-[340px]
       <pre className='mt-2 w-full overflow-x-auto rounded-md bg-slate-950 p-4'>
         <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
       </pre>
